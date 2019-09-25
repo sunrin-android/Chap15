0d78dd1dc4f8738a43344e22f32f6bf6650378c4
heptacode <admin@hyunwoo.kim>
2019-09-25 10:24:45 +0900

    Update Module Dount

---
+++ test15/src/main/java/com/example/test15/Donut.java
@@ -0,0 +1,18 @@
+package com.example.test15;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class Donut extends View {
+
+    public Donut(Context context) {
+        super(context);
+    }
+
+    public Donut(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
